f5f041cf575d493e76c842d63bfb173621142f82
victorrg99 <44617510+victorrg99@users.noreply.github.com>
2019-04-07 15:49:44 +0200

    Commit 3 - Victor Rivero Gómez

--- ProyectoFinal-ERP-Academia/ProyectoFinal-ERP-Academia/Views/Form1.cs
+++ ProyectoFinal-ERP-Academia/ProyectoFinal-ERP-Academia/Views/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using ProyectoFinal_ERP_Academia.Conexion;
 using ProyectoFinal_ERP_Academia.Util.Clases;
+using ProyectoFinal_ERP_Academia.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,37 +15,17 @@ namespace ProyectoFinal_ERP_Academia
 {
     public partial class Form1 : Form
     {
-        private static List<Rol> roleTable;
-        public static List<Rol> RoleList
-        {
-            get { return roleTable; }
-            set { roleTable = value; }
-        }
-        public void AddRoles(DataSet data)
-        {
-            List<Rol> ps = new List<Rol>();
-
-            foreach (DataRow dr in data.Tables["ROLES"].Rows)
-            {
-                ps.Add(new Rol(dr["ROL"].ToString()));
-            }
-
-            roleTable = ps;
-        }
-        public void getRoles()
-        {
-            DataSet data = new DataSet();
-            ConnectOracle connection = new ConnectOracle();
-            data = connection.getData("SELECT DISTINCT ROL, ID_ROL FROM ROLES ORDER BY ID_ROL", "ROLES");
-            AddRoles(data);
-        }
+        
+        
         public Form1()
         {
             InitializeComponent();
-            getRoles();
-            RoleList.ForEach(x => this.comboBox1.Items.Add(x.NombreRol));
+        }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AgregarUsuario au = new AgregarUsuario();
+            au.ShowDialog();
         }
     }
 }
